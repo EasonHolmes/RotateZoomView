2e46ad5e1df3ba74f747eff8c7089664febd5a50
CuiYang <holmeseason@gmail.com>
2016-02-19 15:51:22 +0800

    f

--- app/src/main/java/com/practice/noyet/rotatezoomimageview/MainActivity.java
+++ app/src/main/java/com/practice/noyet/rotatezoomimageview/MainActivity.java
@@ -1,23 +1,10 @@
 package com.practice.noyet.rotatezoomimageview;
 
 import android.app.Activity;
-import android.graphics.Bitmap;
-import android.graphics.BitmapFactory;
-import android.graphics.Matrix;
-import android.graphics.PointF;
-import android.graphics.RectF;
-import android.os.AsyncTask;
 import android.os.Bundle;
-import android.util.DisplayMetrics;
-import android.view.MotionEvent;
-import android.view.View;
 import android.widget.ImageView;
 
 import com.squareup.picasso.Picasso;
-import com.ypy.eventbus.EventBus;
-
-import java.io.File;
-import java.math.BigDecimal;
 
 public class MainActivity extends Activity {
 
